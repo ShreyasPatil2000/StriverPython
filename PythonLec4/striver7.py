8f3f66135059e4129c8ac4ed9ec8849444454e6b
RaydenDarkus <svpatil20000@gmail.com>
2025-11-05 19:31:33 +0530

    5th November, 2025 update 2

--- PythonLec4/striver7.py
+++ PythonLec4/striver7.py
@@ -1,9 +1,13 @@
 class Solution:
     def isPrime(self, n: int) -> bool:
         #your code goes here
+        if n <= 1:
+            return False
+        for i in range(2, n // 2):
+            if n % i == 0:
+                return False 
         return True
         
-    
 if __name__ == "__main__":    
     ob = Solution()
-    print(ob.isPrime(153))
+    print(ob.isPrime(97))
