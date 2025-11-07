63f888b13ab7ec0194c99b8797140108bee10cc4
RaydenDarkus <svpatil20000@gmail.com>
2025-11-07 16:52:59 +0530

    7th November, 2025 update 2

--- SortingLec1/insertion_sort.py
+++ SortingLec1/insertion_sort.py
@@ -3,9 +3,13 @@ from typing import List
 class Solution:
     def insertionSort(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        
+        for i in range(1, n):
+            j = i
+            while j > 0 and nums[j - 1] > nums[j]:
+                nums[j], nums[j - 1] = nums[j - 1], nums[j]
+                j -= 1
         return nums
         
 if __name__ == "__main__":    
     ob = Solution()
-    print(ob.bubbleSort([13, 46, 24, 52, 20, 9]))
+    print(ob.insertionSort([13, 46, 24, 52, 20, 9]))
---
+++ SortingLec2/quick_sort.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def quickSort(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        return nums
+        
+if __name__ == "__main__":    
+    ob = Solution()
+    print(ob.quickSort([13, 46, 24, 52, 20, 9]))
