86f86154ba2960e275b9658bd2065f6af6910d03
RaydenDarkus <svpatil20000@gmail.com>
2025-11-09 20:33:06 +0530

    9th November, 2025 update 1

--- SortingLec2/merge_sort.py
+++ SortingLec2/merge_sort.py
@@ -2,8 +2,28 @@ from typing import List
 
 class Solution:
     def mergeSort(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        return nums
+        if len(nums) <= 1:
+            return nums
+        mid = len(nums) // 2
+        left = nums[: mid]
+        right = nums[mid: ]
+        sortedLeft = self.mergeSort(left)
+        sortedRight = self.mergeSort(right)
+        return self.merge(sortedLeft, sortedRight)
+    
+    def merge(self, left: List[int], right: List[int]) -> List[int]:
+        result = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
         
 if __name__ == "__main__":    
     ob = Solution()
--- SortingLec2/quick_sort.py
+++ SortingLec2/quick_sort.py
@@ -1,10 +1,34 @@
 from typing import List
 
 class Solution:
-    def quickSort(self, nums: List[int]) -> List[int]:
-        n = len(nums)
+    
+    def swap(self, nums: List[int], i: int, j: int):
+        nums[i], nums[j] = nums[j], nums[i]
+        
+    def partition(self, nums: List[int], low: int, high: int) -> int:
+        pivot = nums[low]
+        i = low
+        j = high
+        while i < j: 
+            while nums[i] <= pivot and i <= high - 1:
+                i += 1
+            while nums[j] > pivot and j >= low + 1:
+                j -= 1
+            if i < j: 
+                self.swap(nums, i, j)
+        self.swap(nums, low, j)
+        return j
+    
+    def quickSort(self, nums: List[int], low: int, high: int) -> List[int]:
+        if(low < high):
+            pi = self.partition(nums, low, high)
+            self.quickSort(nums, low, pi - 1)
+            self.quickSort(nums, pi + 1, high)
         return nums
         
 if __name__ == "__main__":    
     ob = Solution()
-    print(ob.quickSort([13, 46, 24, 52, 20, 9]))
+    nums = [13, 46, 24, 52, 20, 9]
+    low = 0
+    high = len(nums) - 1
+    print(ob.quickSort(nums, low, high))
